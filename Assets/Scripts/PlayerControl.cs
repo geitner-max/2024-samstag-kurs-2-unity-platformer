092e6cf394a7495cd3486cf16e9b75b352d52a89
Informa-Tiger <67505689+Informa-Tiger@users.noreply.github.com>
2024-07-06 11:31:42 +0200

    Stand 29.06.2024

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float speed;
+    Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+    }
+}
