2787572fc428e61bc7dfb14e7b84ea6dfd2ac6ef
Informa-Tiger <67505689+Informa-Tiger@users.noreply.github.com>
2024-07-12 00:21:33 +0200

    Stand 06.07.2024

---
+++ Assets/Scripts/FeetControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeetControl : MonoBehaviour
+{
+    public LayerMask platforms;
+    private bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(GetComponent<Transform>().position.y);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        if (other.gameObject.layer == platforms) grounded = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+         Debug.Log(other);
+        if (other.gameObject.layer == platforms) grounded = false;
+    }
+
+    public bool IsGrounded() { return grounded; }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,17 +6,20 @@ public class PlayerControl : MonoBehaviour
 {
 
     public float speed;
+    public float jumpPower;
+    public FeetControl feet;
     Rigidbody2D body;
 
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();     
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        if (Input.GetKey(KeyCode.Space) && feet.IsGrounded()) body.velocity = new Vector2(body.velocity.x, jumpPower);
     }
 }
